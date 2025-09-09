9c616f8fcde7706b58ad575476cc4fdb9baa9940
ZOUEL2 <zouel2@oocl.com>
2025-09-09 19:16:31 +0800

    feat: find employee by id

--- src/main/java/org/example/springboottest/controller/EmployeeController.java
+++ src/main/java/org/example/springboottest/controller/EmployeeController.java
@@ -1,33 +1,45 @@
 package org.example.springboottest.controller;
 
 import org.example.springboottest.po.Employee;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
 
-    List<Employee> employees = new ArrayList<>();
+    private final List<Employee> employees = new ArrayList<>();
+    private final AtomicLong idGenerator = new AtomicLong(0);
 
     @PostMapping
     public ResponseEntity<Map<String, Object>> createEmployee(@RequestBody Employee employee) {
-        employee.setId(employees.size() + 1);
+        long id = idGenerator.incrementAndGet();
+        employee.setId(id);
         employees.add(employee);
-        return ResponseEntity.status(201).body(Map.of("id", employee.getId()));
+        return ResponseEntity.status(HttpStatus.CREATED).body(Map.of("id", employee.getId()));
     }
 
     @GetMapping("/{id}")
-    public Employee getEmployee(@PathVariable long id) {
-        return employees.stream().filter(employee -> employee.getId() == id).findFirst().orElse(null);
+    public ResponseEntity<Employee> getEmployee(@PathVariable long id) {
+        return employees.stream().filter(employee -> employee.getId() == id)
+                .findFirst()
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
 
     @GetMapping
     public List<Employee> queryEmployeeByGender(@RequestParam String gender) {
-        return employees.stream().filter(employee -> employee.getGender() == gender).toList();
+        String formatGender = gender.toLowerCase(Locale.ROOT);
+        return employees.stream()
+                .filter(e -> e.getGender() != null &&
+                        e.getGender().toLowerCase(Locale.ROOT).equals(formatGender))
+                .toList();
     }
 }
---
+++ src/test/java/org/example/springboottest/EmployeeTest.java
@@ -0,0 +1,66 @@
+package org.example.springboottest;
+
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+class EmployeeTest {
+
+    @Resource
+    private MockMvc mockMvc;
+
+    private String employeeJson(String name, String gender, int age, double salary) {
+        return """
+                {
+                  "name":"%s",
+                  "gender":"%s",
+                  "age":%d,
+                  "salary":%s
+                }
+                """.formatted(name, gender, age, salary);
+    }
+
+    @Test
+    void should_create_employee_when_post_given_a_valid_body() throws Exception {
+        mockMvc.perform(post("/employees")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(employeeJson("Tom", "Male", 18, 5000.0))).andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value( 1));
+
+        mockMvc.perform(post("/employees")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(employeeJson("Tom", "Male", 18, 5000.0))).andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value( 2));
+    }
+
+    @Test
+    void should_find_employee_when_given_id() throws Exception {
+        mockMvc.perform(post("/employees")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(employeeJson("Tom", "Male", 18, 5000.0)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value(1));
+
+        mockMvc.perform(get("/employees/{id}", 1))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("Tom"))
+                .andExpect(jsonPath("$.age").value(18))
+                .andExpect(jsonPath("$.gender").value("Male"))
+                .andExpect(jsonPath("$.salary").value(5000.0));
+    }
+
+}
